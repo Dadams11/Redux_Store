a33c11709fd2436181156a7172221f941708968c
Dadams11 <adams.1743@osu.edu>
2023-10-04 02:20:09 -0400

    updated files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
+const { ApolloServer } = require('@apollo/server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
@@ -12,10 +11,10 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware
 });
 
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async () => {
+async function startServer() {
   await server.start();
 
   app.use(express.urlencoded({ extended: false }));
@@ -24,13 +23,13 @@ const startApolloServer = async () => {
   // Serve up static assets
   app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-  }));
-
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
+  }
 
+  server.applyMiddleware({ app });
+
+  if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
@@ -39,10 +38,9 @@ const startApolloServer = async () => {
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
   });
-};
+}
 
-// Call the async function to start the server
-startApolloServer();
+startServer();
